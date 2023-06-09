424c7a5d1cb7e6c2627a19c177b35fcd6da079f2
Riccumeni <77925342+Riccumeni@users.noreply.github.com>
2023-06-09 12:51:23 +0200

    some changes

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,65 +4,79 @@ import {colors} from "../assets/colors";
 import {Colors} from "react-native/Libraries/NewAppScreen";
 import axios from "axios";
 import HTMLView from 'react-native-htmlview';
+import TagCard from "../components/TagCard";
+import { FontAwesome } from '@expo/vector-icons';
 
 
 export const GameScreen = () => {
 
     let [game, setGame] = useState({});
+    let [image, setImage] = useState([]);
 
     useEffect(() => {
-        axios.get("https://api.rawg.io/api/games/3498?key=3f0a855ff4384b05af50094b2c218aaf")
+        axios.get("https://api.rawg.io/api/games/292842?key=3f0a855ff4384b05af50094b2c218aaf")
             .then(result => {
                 setGame(() => result.data);
             })
+
+        axios.get("https://api.rawg.io/api/games/292842/screenshots?key=3f0a855ff4384b05af50094b2c218aaf")
+            .then(result => {
+                setImage(() => result.data.results);
+            })
     }, [])
     return(
-        <ImageBackground style={styles.container} source={{uri: game.background_image}}>
-            <View style={{position: "absolute", width: "100%", height: "100%", backgroundColor: colors.background, top: 0, left: 0, opacity: 0.90}}></View>
+        <ImageBackground style={styles.container} source={{uri: game.background_image_additional}}>
+            <View style={{position: "absolute", width: "100%", height: "100%", backgroundColor: colors.background, top: 0, left: 0, opacity: 0.95}}></View>
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={{flexDirection: "row", justifyContent: "space-evenly", marginVertical: 20}}>
+                <Image source={{uri: game?.background_image}} style={{width: "100%", height: 200}}/>
+                <View style={{flexDirection: "row", justifyContent: "center", marginVertical: 20}}>
                     <Text style={styles.title}>{game.name}</Text>
-                    <View style={{width: 30, height: 30, backgroundColor: "#D2E4FF", borderRadius: 15, alignSelf: "flex-end"}}></View>
                 </View>
 
                 <View style={{flexDirection: "row", justifyContent: "center", gap: 20, marginVertical: 20}}>
-                    <View style={{width: 30, height: 30, backgroundColor: "#D2E4FF", borderRadius: 15, alignSelf: "flex-end"}}></View>
-                    <Text style={styles.iconText}>{game.rating}</Text>
+                    <FontAwesome style={{alignSelf: "flex-end"}} name="star" size={24} color="#FFB800" />
+                    <Text style={styles.iconText}>{game.rating ? game.rating : "not rated"}</Text>
                     <Text style={{...styles.iconText, fontSize: 18, transform: [{rotateZ: "45deg"}]}}>m</Text>
-                    <Text style={styles.iconText}>{game.metacritic}</Text>
+                    <Text style={styles.iconText}>{game.metacritic ? game.metacritic : "not rated"}</Text>
                 </View>
 
-                <HTMLView stylesheet={htmlStyles} value={game.description.split("<br />")[0]}/>
+                <Text style={styles.description}>{game.description_raw?.split(".")[0] + game.description_raw?.split(".")[0] + "."}</Text>
 
                 <Text style={styles.titleSection}>Genre</Text>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginLeft: 20}}>
-                    <View style={{width: 160, height: 35, backgroundColor: "#D2E4FF30", borderRadius: 5, flexDirection: "row", justifyContent: "space-evenly"}}>
-                        <View style={{width: 20, height: 20, backgroundColor: "white", borderRadius: 15, alignSelf: "center"}}></View>
-                        <Text style={styles.iconText}>Adventure</Text>
-                    </View>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{paddingLeft: 20}}>
+                    {
+                        game.genres?.map((genre) => {
+                            return <TagCard name={genre.name}/>
+                        })
+                    }
                 </ScrollView>
 
                 <Text style={styles.titleSection}>Platforms</Text>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginLeft: 20}}>
-                    <View style={{width: 160, height: 35, backgroundColor: "#D2E4FF30", borderRadius: 5, flexDirection: "row", justifyContent: "space-evenly"}}>
-                        <View style={{width: 20, height: 20, backgroundColor: "white", borderRadius: 15, alignSelf: "center"}}></View>
-                        <Text style={styles.iconText}>Adventure</Text>
-                    </View>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{paddingLeft: 20}}>
+                    {
+                        game.platforms?.map((platform) => {
+                            return <TagCard name={platform.platform.name}/>
+                        })
+                    }
                 </ScrollView>
 
                 <Text style={styles.titleSection}>Tags</Text>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginLeft: 20}}>
-                    <View style={{width: 160, height: 35, backgroundColor: "#D2E4FF30", borderRadius: 5, flexDirection: "row", justifyContent: "space-evenly"}}>
-                        <View style={{width: 20, height: 20, backgroundColor: "white", borderRadius: 15, alignSelf: "center"}}></View>
-                        <Text style={styles.iconText}>Adventure</Text>
-                    </View>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{paddingLeft: 20}}>
+                    {
+                        game.tags?.map((tag) => {
+                            return <TagCard name={tag.name}/>
+                        })
+                    }
                 </ScrollView>
 
                 <Text style={styles.titleSection}>Screenshots</Text>
-                <ScrollView horizontal={true} style={{marginLeft: 20, marginBottom: 20}} showsHorizontalScrollIndicator={false}>
-                    <Image source={{uri: "https://media.rawg.io/media/crop/600/400/games/556/55684bfd048706f4266d331d70050b37.jpg"}} style={{width: 250, height: 125, borderRadius: 20, marginRight: 20}}/>
-                    <Image source={{uri: "https://media.rawg.io/media/crop/600/400/games/556/55684bfd048706f4266d331d70050b37.jpg"}} style={{width: 250, height: 125, borderRadius: 20, marginRight: 20}}/>
-                    <Image source={{uri: "https://media.rawg.io/media/crop/600/400/games/556/55684bfd048706f4266d331d70050b37.jpg"}} style={{width: 250, height: 125, borderRadius: 20, marginRight: 20}}/>
+                <ScrollView horizontal={true} style={{marginBottom: 20, paddingLeft: 20}} showsHorizontalScrollIndicator={false}>
+                    {
+                        image?.map((image) => {
+                            return <Image source={{uri: image.image}} style={{width: 300, height: 170, borderRadius: 20, marginRight: 20}}/>
+
+                        })
+                    }
                 </ScrollView>
             </ScrollView>
         </ImageBackground>
@@ -95,18 +109,9 @@ const styles = StyleSheet.create({
     },
     titleSection: {
         color: "white",
-        fontSize: 24,
+        fontSize: 28,
         fontWeight: "600",
         marginLeft: 20,
-        marginVertical: 20
-    }
-})
-
-const htmlStyles = StyleSheet.create({
-    p : {
-        color: "white",
-        marginHorizontal: 20,
-        fontSize: 16,
         marginVertical: 20
     }
 })
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,19 +2,42 @@ import {StyleSheet, View, Text, ScrollView} from "react-native";
 import {colors} from "../assets/colors";
 import GameCard from "../components/GameCard";
 import {Blur, Canvas, Image, useImage, SkiaView} from "@shopify/react-native-skia";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export const HomeScreen = () => {
-    const image = useImage('https://picsum.photos/200/300');
-    if (!image) {
-        return null;
+
+    const [game, setGame] = useState([]);
+
+    let day = new Date().getDate().toString();
+    let month = new Date().getMonth().toString();
+    let year = new Date().getFullYear().toString();
+
+    if(day.length === 1){
+        day = "0" + day;
     }
+
+    if(month.length === 1){
+        month = "0" + month;
+    }
+
+    // 2010-01-01
+    useEffect(() => {
+        axios.get(`https://api.rawg.io/api/games?dates=2023-06-10,2023-07-10&ordering=-added&key=3f0a855ff4384b05af50094b2c218aaf`)
+            .then(result => {
+                setGame(result.data.results)
+            })
+    }, [])
+
     return (
         <View style={styles.container}>
             <Text style={styles.comingGamesText}>Next Coming Games</Text>
             <ScrollView horizontal={true}>
-                <GameCard name="Zelda: Tears of Kingdom" urlImage="https://media.rawg.io/media/crop/600/400/games/556/55684bfd048706f4266d331d70050b37.jpg"/>
-                <GameCard name="Zelda: Tears of Kingdom" urlImage="https://media.rawg.io/media/crop/600/400/games/556/55684bfd048706f4266d331d70050b37.jpg"/>
-                <GameCard name="Zelda: Tears of Kingdom" urlImage="https://media.rawg.io/media/crop/600/400/games/556/55684bfd048706f4266d331d70050b37.jpg"/>
+                {
+                    game.map(game => {
+                        return <GameCard name={game?.name} urlImage={game?.background_image}/>
+                    })
+                }
             </ScrollView>
         </View>
     );
